3a1eb25cf1c8ef87c19bd821a4427a6cb6deae9d
Erratic Motion <robert.ives@orange.fr>
2019-01-19 09:49:24 +0000

    Support multiple frameworks

---
+++ Gherkin.Net/Gherkin/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Erratic Motion Ltd">
+// Copyright (c) Erratic Motion Ltd. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Gherkin.Tests")]
